1a0b11a2e99d41dc38bca818715911397e351b83
James Cox <j-cox03@hotmail.com>
2017-05-27 21:07:27 +0100

    Made initial tests for pizza base class

--- decorator_pattern/ruby/pizza_decorator/pizza_decorator_spec.rb
+++ decorator_pattern/ruby/pizza_decorator/pizza_decorator_spec.rb
@@ -4,6 +4,24 @@ require_relative("pizza_decorator.rb")
 
 class TestPizzaDecorator < Minitest::Test
 
-  
+  def test_pizza_base_stonebake__get_description
+    result = get_pizza_base(0).get_description()
+    assert_equal("Stonebake Pizza", result)
+  end
+
+  def test_pizza_base_deep_pan__get_description
+    result = get_pizza_base(1).get_description()
+    assert_equal("Deep Pan Pizza", result)
+  end
+
+  def test_pizza_base_stonebake__get_cost
+    result = get_pizza_base(0).get_cost()
+    assert_equal(4, result)
+  end
+
+  def test_pizza_base_deep_pan__get_cost
+    result = get_pizza_base(1).get_cost()
+    assert_equal(6, result)
+  end
 
 end
