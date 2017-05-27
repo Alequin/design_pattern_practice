c1490caf9f9281a3f335934da129ff99c12bf366
James Cox <j-cox03@hotmail.com>
2017-05-27 22:27:10 +0100

    Passed tests for pizza with sweetcore and chorizo

--- decorator_pattern/ruby/pizza_decorator/pizza_decorator.rb
+++ decorator_pattern/ruby/pizza_decorator/pizza_decorator.rb
@@ -75,9 +75,28 @@ class Sweetcorn < Topping
 
 end
 
+class Chorizo < Topping
+
+  def get_description()
+    return "#{@description} chorizo"
+  end
+
+  def get_cost
+    return @pizza.get_cost() + 3
+  end
+
+end
+
 def get_stonebake_with_sweetcorn()
   pizza = get_pizza_base(0)
   pizza = Sweetcorn.new(pizza)
+  return pizza
+end
+
+def get_stonebake_with_sweetcorn_chorizo()
+  pizza = get_pizza_base(1)
+  pizza = Sweetcorn.new(pizza)
+  pizza = Chorizo.new(pizza)
   return pizza
 end
 
